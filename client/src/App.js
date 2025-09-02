731d8f96ceea1ad10840b8a594a6caffced0bbb8
Arun Samy <134082910+arunsamy4444@users.noreply.github.com>
2025-09-02 12:48:46 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
